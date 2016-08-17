b02b5a1a9c70d5896c9a51d330a47b9f8d3229a8
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-08-17 16:38:45 +0200

    converter script first working version

--- python/fastq2fasta.py
+++ python/fastq2fasta.py
@@ -1,1 +1,76 @@
-# TODO
+# Functions to transform fastq files to fasta files
+# by cutting low quality reads (marked with # in the quality line)
+# in our sample files with BASE=33, see  http://drive5.com/usearch/manual/quality_score.html
+
+from datetime import datetime
+
+MinSeqLen = 60
+
+def transformHeaderSimple(headerStr):
+  return ">" + headerStr[1:]
+
+def transformHeader(headerStr, n, origLen):
+  pos = headerStr.rfind('=' + str(origLen))
+  newHeader = ">" + headerStr[1:pos +1] + str(origLen - n)
+  return newHeader
+
+def cutSeq(seq, n):
+  until = len(seq) - n
+  return seq[0:until]
+
+def convertFastqItem2FastaItem(inputLines, minSeqLen = MinSeqLen):
+  # there should be 4 input lines
+  if (len(inputLines) != 4 or inputLines[0][0] != '@'):
+    return "ERROR: WRONG INPUT FORMAT"
+  header = inputLines[0]
+  sequence = inputLines[1]
+  qualityStr = inputLines[3]
+  origLen= len(sequence)
+  cnt = 0
+  for c in reversed(qualityStr):
+    if c != '#':
+      break
+    else:
+      cnt += 1
+  if (origLen - cnt < 60):
+    return "TOO LOW SEQUENCE QUALITY : " + header
+  if (cnt > 0):
+    return [transformHeader(header, cnt, origLen), cutSeq(sequence, cnt)]
+  else:
+    return [transformHeaderSimple(header), sequence]
+
+def convertFastqFile2FastaFile(inFileName, outFileName, logFileName):
+  infile = open(inFileName, 'r')
+  outfile = open(outFileName, 'w')
+  logfile = open(logFileName, 'w')
+  # read input file in 4-line batches
+  itemCounter = 0
+  okCounter = 0
+  linesToProcess = []
+  for line in infile:
+    linesToProcess.append(line.strip())
+    if (len(linesToProcess) == 4):
+      itemCounter += 1
+      result = convertFastqItem2FastaItem(linesToProcess)
+      if (len(result) != 2):
+        logfile.write(str(datetime.now()) + "  -  " + result + "\n")
+      else:
+        okCounter += 1
+        for resLine in result:
+          outfile.write(resLine + '\n')
+      linesToProcess = []
+  
+  print("Items processed: " + str(itemCounter))
+  infile.close()
+  outfile.close()
+  logfile.close()
+
+
+def main():
+  convertFastqFile2FastaFile('../sample_data/sample1.fastq',
+       '../sample_data/sample1_result.fasta',
+       '../sample_data/sample1_logs.txt')
+
+
+if __name__ == "__main__":
+  main()
--- python/test_fastq2fasta.py
+++ python/test_fastq2fasta.py
@@ -1,8 +1,55 @@
+from fastq2fasta import *
 
 def test_read_fastq():
-  in_data = open('../sample_data/sample1.fastq', 'r')
+  inData = open('../sample_data/sample1.fastq', 'r')
   counter = 0
-  for line in in_data:
+  for line in inData:
     counter += 1
   assert(200, counter)
+
+def test_cut_seq():
+  sequence = "123456"
+  assert("1234" == cutSeq(sequence, 2))
+  assert("123456" == cutSeq(sequence, 0))
+  assert("" == cutSeq(sequence, 6))
+
+def test_transform_header_simple():
+  header = "@lorem ipsum dolor length=60"
+  expHeader1 = ">lorem ipsum dolor length=60"
+  assert(expHeader1 == transformHeaderSimple(header))
+
+def test_transform_header():
+  header = "@lorem ipsum dolor length=60"
+  expHeader1 = ">lorem ipsum dolor length=49"
+  assert(expHeader1 == transformHeader(header, 11, 60))
+  expHeader2 = ">lorem ipsum dolor length=40"
+  assert(expHeader2 == transformHeader(header, 20, 60))
+
+def test_trim_bad_endings1():
+  oneFastqItem = [ "@ERR445333.1.2 1 length=30",
+        "AACCCCTAACCCCTAACCCTAACCCTAACC",
+        "+ERR445333.1.2 1 length=30",
+        "EFFFGF?EFGA???DC>:?AEEEAAAAAAA" ]
+  resultLines = convertFastqItem2FastaItem(oneFastqItem, 10)
+  assert(2, len(resultLines))
+  assert('>ERR445333.1.2 1 length=30' == resultLines[0])
+  assert(30, len(resultLines[1])) # nothing should be cut
+
+def test_trim_bad_endings2():
+  oneFastqItem = [ "@ERR445333.1.2 1 length=30",
+        "AACCCCTAACCCCTAACCCTAACCCTAACC",
+        "+ERR445333.1.2 1 length=30",
+        "EFFFGF?EFGA???DC>:?A##########" ]
+  resultLines = convertFastqItem2FastaItem(oneFastqItem, 10)
+  assert(2, len(resultLines))
+  assert('>ERR445333.1.2 1 length=20' == resultLines[0])
+  assert(20, len(resultLines[1])) # last 10 chars cut 
+
+def test_trim_all_bad():
+  oneFastqItem = [ "@ERR445333.1.2 1 length=30",
+        "AACCCCTAACCCCTAACCCTAACCCTAACC",
+        "+ERR445333.1.2 1 length=30",
+        "##############################" ]
+  resultLines = convertFastqItem2FastaItem(oneFastqItem, 10)
+  assert("TOO LOW SEQUENCE QUALITY", resultLines)
 
